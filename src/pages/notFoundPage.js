eaeb508ef59e8d084f6bd5f337f1b26812a42856
orlovse <hajoff@gmail.com>
2021-01-14 21:49:12 +0200

    styled 404 page

--- src/pages/notFoundPage.js
+++ src/pages/notFoundPage.js
@@ -1,14 +1,21 @@
 import styles from "./pages.module.css";
 import { Link } from "react-router-dom";
+import NotFound from "../components/404";
+import { Hidden } from "@material-ui/core";
 const NotFoundPage = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.text}>
-        <div>404 Page not found</div>
-        <Link to="/" className={styles.link}>
-          Go to home page
-        </Link>
-      </div>
+      <Hidden smDown>
+        <NotFound />
+      </Hidden>
+      <Hidden mdUp>
+        <div className={styles.text}>
+          <div>404 Page not found</div>
+          <Link to="/" className={styles.link}>
+            Go to home page
+          </Link>
+        </div>
+      </Hidden>
     </div>
   );
 };
