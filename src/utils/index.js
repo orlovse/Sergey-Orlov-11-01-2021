0be61c2fa5f2c158f523c979f85b0ae8fbed9d03
orlovse <hajoff@gmail.com>
2021-01-15 19:29:59 +0200

    delete unussed function from utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,21 +1,3 @@
-export const getIconName = (number) => {
-  if (number > 0 && number < 6) {
-    return "sunny";
-  } else if ((number > 5 && number < 12) || (number > 34 && number < 39)) {
-    return "cloudy";
-  } else if (number > 11 && number < 15) {
-    return "cloud-sun-rays";
-  } else if ((number > 14 && number < 18) || (number > 40 && number < 43)) {
-    return "thunder-storm";
-  } else if (number === 18 || (number > 38 && number < 41)) {
-    return "rainy";
-  } else if ((number > 18 && number < 30) || number > 42) {
-    return "flurries";
-  } else {
-    return "cloudy";
-  }
-};
-
 export const loadFromLocalStorage = (key) => {
   try {
     const serialisedState = localStorage.getItem(key);
